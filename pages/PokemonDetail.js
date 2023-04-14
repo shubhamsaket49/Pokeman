5c282d815513e63a211b0ec0cf55a6ea7b38d5d3
Shubham Saket <63151463+shubhamsaket49@users.noreply.github.com>
2023-04-15 00:13:12 +0530

    Add files via upload

---
+++ pages/PokemonDetail.js
@@ -0,0 +1,78 @@
+import React, { useState } from 'react';
+import { useQuery, gql } from '@apollo/client';
+
+import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+
+
+// Create a new Apollo Client instance
+const client = new ApolloClient({
+    link: createHttpLink({ uri: 'https://graphql-pokemon2.vercel.app/' }),
+    cache: new InMemoryCache(),
+});
+
+// Define your GraphQL query
+const GET_POKEMON_QUERY = gql`
+  query getPokemon($name: String!) {
+    pokemon(name: $name) {
+      id
+      name
+      image
+      height
+      weight
+      classification
+      types
+      weaknesses
+      resistances
+      evolutions {
+        id
+        name
+        image
+      }
+    }
+  }
+`;
+
+// Fetch data from the API
+client.query({
+    query: GET_POKEMON_QUERY,
+    variables: { name: 'pikachu' }, // Pass any required variables
+})
+    .then(result => {
+        console.log(result.data.pokemon);
+    })
+    .catch(error => {
+        console.error(error);
+    });
+
+
+
+const PokemonDetail = ({ name }) => {
+    const [showEvolutions, setShowEvolutions] = useState(false);
+    const { loading, error, data } = useQuery(GET_POKEMON, {
+        variables: {
+            name,
+        },
+    });
+
+    if (loading) return <div>Loading...</div>;
+    if (error) return <div>Error: {error.message}</div>;
+
+    const { id, image, height, weight, classification, types, weaknesses, resistances, evolutions } = data.pokemon;
+
+    const handleShowEvolutions = () => {
+        setShowEvolutions(!showEvolutions);
+    };
+
+    return (
+        <div>
+            <h1>Pokemon Detail</h1>
+            <img src={image} alt={name} />
+            <p>Number: {id}</p>
+            <p>Name: {name}</p>
+            <p>Height: {height}</p>
+            <p>Weight: {weight}</p>
+            <p>Classification: {classification}</p>
+            <p>Types: {types.join(', ')}</p>
+        </div>
+    )
+}
